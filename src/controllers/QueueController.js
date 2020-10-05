2adb5d7662c1c8e0604a508cf76e1cc0a1f6da7b
Jorge Dias <jorgediasdsg@gmail.com>
2020-10-05 09:05:48 -0300

    🚀 chore: Add queue list

--- src/controllers/QueueController.js
+++ src/controllers/QueueController.js
@@ -10,24 +10,27 @@ module.exports = {
             queue = JSON.parse(body)
             if (!error && response.statusCode == 200) {
                 agents = queue.agents;
+                // Verifica e cadastra os online
                 agents.forEach(async function (agent, array) {
                     agent = {
                         extension: agent.extension,
                         calls_today: agent.calls_today,
                         last_call: agent.last_call
                     }
-                    const agentOnline = Queue.findOne({extension: agent.extension})
-                    console.log(agentOnline)
+                    const agentOnline = await Queue.findOne({extension: agent.extension})
+                    
+                    // console.log(agentOnline)
                     if(agentOnline){
                         console.log(`${agent.extension}  está conectado`)
                     } else {
-                        console.log(`${agent.extension}  Conectou agora!`)
-                        // const res = User.create(agent);
+                        console.log(`${agent.extension}  conectou agora!`)
+                        const res = Queue.create(agent);
                     }
-                    response.json({message: "Lista de usuários atualizada!", res})      
                 })
-                // const agentsOnline = Queue.find();
-                // return response.json(agentsOnline);  
+                response.json({message: "Lista de usuários atualizada!", res})      
+                const agentsOnline = Queue.find();  
+                console.log(agentsOnline);
+                // return response.json(agentsOnline);        
             } else {
                 return    
             }
